6588992a0e6c35749cda8103c1251da5f8eec156
mahiuddinhabib <mh.habib137@gmail.com>
2023-07-16 15:34:24 +0600

    filter and search option  done

--- src/components/MySidebar.tsx
+++ src/components/MySidebar.tsx
@@ -1,7 +1,21 @@
+import { setFilter } from "@/redux/features/filters/filterSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
-import SelectOption from "./SelectOption";
 
 const MySidebar = () => {
+  const dispatch = useAppDispatch();
+  const [genre, setGenre] = useState("");
+  const [publicationDate, setPublicationDate] = useState("");
+  const handleFilterChange = (key: string, value: string) => {
+    if (key === "genre") {
+      setGenre(value);
+    } else if (key === "publicationDate") {
+      setPublicationDate(value);
+    }
+
+    dispatch(setFilter({ key, value }));
+  };
   return (
     <div
       className={`inset-y-0 left-0 bg-gray-200 transition-transform duration-300 transform relative translate-x-0 w-auto p-0`}
@@ -20,11 +34,33 @@ const MySidebar = () => {
       </li>
 
       {/* Dropdown Select Options */}
-      <select className="border border-gray-300 rounded p-2 mb-4">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-      </select>
-      <SelectOption></SelectOption>
+      <div>
+        <label htmlFor="genre">Genre:</label>
+        <select
+          id="genre"
+          value={genre}
+          onChange={(e) => handleFilterChange("genre", e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="fantasy">Fantasy</option>
+          <option value="romance">Romance</option>
+          {/* Add more genre options */}
+        </select>
+
+        <label htmlFor="publicationDate">Publication Date:</label>
+        <select
+          id="publicationDate"
+          value={publicationDate}
+          onChange={(e) =>
+            handleFilterChange("publicationDate", e.target.value)
+          }
+        >
+          <option value="">All</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+          {/* Add more publication date options */}
+        </select>
+      </div>
 
       {/* Button */}
       <button className="bg-blue-500 text-white rounded p-2">Button</button>
--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,47 +1,80 @@
-import MySidebar from "@/components/MySidebar";
 import TableRow from "@/components/TableRow";
 import { useGetBooksQuery } from "@/redux/features/books/bookApi";
+import { setFilter, setSearchTerm } from "@/redux/features/filters/filterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import { IBook } from "@/types/globalTypes";
-import { Sidebar, Table } from "flowbite-react";
+import { Table } from "flowbite-react";
+import { BiSearchAlt } from "react-icons/bi";
 
 const AllBooks = () => {
-  const { data, isLoading, error } = useGetBooksQuery({searchTerm:''});
+  const dispatch = useAppDispatch();
+  const { filters, searchTerm } = useAppSelector((state) => state.filter);
+  const { genre, publicationDate } = filters;
+
+  const { data } = useGetBooksQuery({
+    searchTerm: searchTerm,
+    genre: genre,
+    publicationDate: publicationDate,
+  });
+  
+  const handleFilterChange = (key: string, value: string) => {
+    dispatch(setFilter({ key, value }));
+  };
+  const handleSearchTermChange = (value: string) => {
+    dispatch(setSearchTerm(value));
+  };
+
   // console.log(data.data);
   return (
     <div className="flex justify-evenly mx-4 md:mx-16 lg:mx-30">
-{/*       <Sidebar aria-label="Default sidebar example">
-        <Sidebar.Items>
-          <Sidebar.ItemGroup>
-            <Sidebar.Item href="#">
-              <p>Dashboard</p>
-            </Sidebar.Item>
-            <Sidebar.Item
-              href="#"
-            
-              label="Pro"
-              labelColor="dark"
-            >
-              <p>Kanban</p>
-            </Sidebar.Item>
-            <Sidebar.Item href="#" label="3">
-              <p>Inbox</p>
-            </Sidebar.Item>
-            <Sidebar.Item href="#">
-              <p>Users</p>
-            </Sidebar.Item>
-            <Sidebar.Item href="#">
-              <p>Products</p>
-            </Sidebar.Item>
-            <Sidebar.Item href="#">
-              <p>Sign In</p>
-            </Sidebar.Item>
-            <Sidebar.Item href="#">
-              <p>Sign Up</p>
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-        </Sidebar.Items>
-      </Sidebar> */}
-      <MySidebar></MySidebar>
+      <div
+        className={`inset-y-0 left-0 bg-gray-200 transition-transform duration-300 transform relative translate-x-0 w-auto p-0`}
+      >
+        {/* Search Bar */}
+        <li className="flex bg-white mx-auto h-8 w-full max-w-lg rounded-full pr-3">
+          <input
+            className="h-8 rounded-full w-full text-sm border-0 focus:ring-0 outline-none"
+            type="text"
+            name="search"
+            id="search"
+            value={searchTerm}
+            onChange={(e) => handleSearchTermChange(e.target.value)}
+          />
+          <button onClick={() => handleSearchTermChange("")}>
+            <BiSearchAlt />
+          </button>
+        </li>
+
+        {/* Dropdown Select Options */}
+        <div>
+          <label htmlFor="genre">Genre:</label>
+          <select
+            id="genre"
+            value={filters.genre}
+            onChange={(e) => handleFilterChange("genre", e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="romance">Romance</option>
+          </select>
+
+          <label htmlFor="publicationDate">Publication Date:</label>
+          <select
+            id="publicationDate"
+            value={filters.publicationDate}
+            onChange={(e) =>
+              handleFilterChange("publicationDate", e.target.value)
+            }
+          >
+            <option value="">All</option>
+            <option value="2011">2011</option>
+            <option value="1969">1969</option>
+          </select>
+        </div>
+
+        {/* Button */}
+        <button className="bg-blue-500 text-white rounded p-2">Add New</button>
+      </div>
       <Table hoverable>
         <Table.Head>
           <Table.HeadCell>Title</Table.HeadCell>
